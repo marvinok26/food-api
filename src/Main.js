48ae09aa9b47d53533f6d22af850bcf0f7576489
Marvin <okongomarvin971@gmail.com>
2024-07-30 19:06:08 +0300

    main

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./style.css";
 
 function Main() {
-  return (
-    <div>Main</div>
-  )
+  const [items, setitems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        // console.log(res.data);
+        setitems(res.data.meals);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const itemslist = items.map(({ strMeal, strMealThumb, idMeal }) => {
+    return (
+      <section className="card">
+        <img src={strMealThumb} />
+        <section className="content">
+          <p>{strMeal}</p>
+          <p>#{idMeal}</p>
+        </section>
+      </section>
+    );
+  });
+
+  return <div className="items-container">{itemslist}</div>;
 }
 
-export default Main
+export default Main;
